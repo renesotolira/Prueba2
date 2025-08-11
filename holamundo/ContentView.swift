055673bc48c7f178bb57c40b609f2895ded6f595
Rene <rene.soto01@iest.edu.mx>
2025-08-11 11:54:37 -0700

    texto

--- holamundo/ContentView.swift
+++ holamundo/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+          
+        Text("Hello, world alsjdlkasjdlkjasdkljaskldjasjdlkasjdkljaskldjaskljdklasjdlkjaslkdjla!")
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(Color.white)
+            .border(Color.purple, width: 4)
+            .clipShape( RoundedRectangle( cornerRadius: 25.0))
+            .shadow(color: Color.red,radius: 28)
+            .rotation3DEffect(.degrees(45), axis: (x: 1, y: 0, z: 0))
+            .opacity(0.5)
+            .lineLimit(1)
+            .truncationMode(.middle)
+            .font(.largeTitle)
+            .bold()
+            .fontWeight(.bold)
+            .blur(radius: 0.8)
+            .padding()
     }
 }
 
