c519bbb8a00504ae58bb41f6601fa75defda410d
Rene <rene.soto01@iest.edu.mx>
2025-08-11 11:33:48 -0700

    Initial Commit

---
+++ holamundo/holamundoApp.swift
@@ -0,0 +1,17 @@
+//
+//  holamundoApp.swift
+//  holamundo
+//
+//  Created by rene senoquia on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct holamundoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
